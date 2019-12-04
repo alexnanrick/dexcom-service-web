49b88991ccefe0caaf6b98a36ade88850022bf22
alexnanrick <alex.nanrick@gmail.com>
2019-12-04 23:34:27 +0000

    Remove environments

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  dexcomServiceBaseUrl: 'https://dexcom-service.herokuapp.com/api',
-  dexcomUserName: '',
-  dexcomPassWord: ''
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  dexcomServiceBaseUrl: 'http://localhost:8080/api',
-  dexcomUserName: '',
-  dexcomPassWord: ''
-};
